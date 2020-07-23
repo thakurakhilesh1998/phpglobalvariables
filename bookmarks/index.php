d3772005d75c25012c02deee0bf77b6cfc7cb49b
thakurakhilesh1998 <39956027+thakurakhilesh1998@users.noreply.github.com>
2020-07-23 21:52:07 +0530

    complete

--- bookmarks/index.php
+++ bookmarks/index.php
@@ -1,3 +1,31 @@
+<?php
+$bookmarks="mybookmarks";
+  session_start();
+  if(isset($_POST['web_name']) && isset($_POST['url']))
+  {
+    if(isset($_SESSION[$bookmarks]))
+    {
+      $_SESSION[$bookmarks][$_POST['web_name']]=$_POST['url'];
+    }
+    else
+    {
+      $_SESSION[$bookmarks]=Array($_POST['web_name']=>$_POST['url']);
+    }
+  }
+
+  if(isset($_GET['action']) && $_GET['action']=='delete')
+  {
+    unset($_SESSION[$bookmarks][$_GET['name']]);
+    header('location:index.php');
+  }
+
+  if(isset($_GET['msg']) && $_GET['msg']=="clearall")
+  {
+    unset($_SESSION[$bookmarks]);
+    header('location:index.php');
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,9 +35,15 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
      integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
      <style>
-         .maincontainer{
-             margin-top: 2rem;
-         }
+      .maincontainer{
+        margin-top:2rem;
+
+      }
+
+      .delete
+      {
+        color:red;
+      }
      </style>
 </head>
 <body>
@@ -36,7 +70,7 @@
     <div class="container-fluid maincontainer">
         <div class="row">
             <div class="col-md-7 first" >
-                <form>
+                <form method="POST" action="<?php $_SERVER['PHP_SELF']?>">
                     <div class="form-group">
                         <label for="websitename">Website Name</label>
                         <input id="websitename" class="form-control" type="text" name="web_name">
@@ -44,15 +78,27 @@
 
                     <div class="form-group">
                         <label for="websiteurl">Website Url</label>
-                        <input id="my-input" class="form-control" type="url" name="url">
+                        <input id="my-input" class="form-control" type="text" name="url">
                     </div>
-                    <input type="button" value="Submit" class="btn btn-primary">
-                    </div>
+                    <input type="submit" value="Submit" class="btn btn-primary">
                 </form>
             </div>
+
             <div class="col-md-5 second">
+
+            <?php if(isset($_SESSION[$bookmarks])):?>
+              <ul class="list-group">
+                <?php foreach ($_SESSION[$bookmarks] as $name=>$url):?>
+                <li class="list-group-item">
+                  <a href="<?php echo $url; ?>" target="_blank"><?php echo $name; ?></a>
+                  <a href="index.php?action=delete&name=<?php echo $name;?>" class="delete">[X]</a>
+                </li>
+                <?php endforeach; ?>
+              </ul>
+                <?php endif;?>
             </div>
         </div>
+     </div>
     </div>
 </body>
 </html>
